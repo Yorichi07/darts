5ac1487912ac1d2ad678bc2c411534e9fd5032f9
Aditya07 <devganaditya@gmail.com>
2024-04-03 23:45:58 +0530

    service & repository added

---
+++ src/main/java/com/darts/server/repository/HospitalRepository.java
@@ -0,0 +1,9 @@
+package com.darts.server.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.darts.server.model.Hospital;
+
+public interface HospitalRepository extends CrudRepository<Hospital,Long>{
+    
+
+}
---
+++ src/main/java/com/darts/server/service/HospitalService.java
@@ -0,0 +1,26 @@
+package com.darts.server.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.darts.server.model.Hospital;
+import com.darts.server.repository.HospitalRepository;
+
+@Service
+public class HospitalService {
+    @Autowired
+    HospitalRepository hosRepo;
+
+    //Read All
+    public List<Hospital> getAllHospitals(){
+        return (List<Hospital>) hosRepo.findAll();
+    }
+
+    //Find One
+    public Optional<Hospital> getOneHospital(Long id){
+        return hosRepo.findById(id);
+    }
+}
