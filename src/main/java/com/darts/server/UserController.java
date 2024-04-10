7f5ea0aa7d57338ffe48080fb6e082595b25434c
vibzz2003 <500093004@stu.upes.ac.in>
2024-04-10 11:58:32 +0530

    serving static pages

--- src/main/java/com/darts/server/UserController.java
+++ src/main/java/com/darts/server/UserController.java
@@ -9,13 +9,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.darts.server.functions.CreateQr;
 import com.darts.server.functions.PasswordHash;
@@ -30,15 +25,15 @@ import com.darts.server.service.UserService;
 @RequestMapping("/api/user")
 public class UserController {
 
-    //user Service
+    // User Service
     @Autowired
     UserService userService;
 
-    //Patient Service
+    // Patient Service
     @Autowired
     Patient_detailsService patientService;
 
-    //private key
+    // Private key
     @Value("${secrets.secretkey}")
     private String secretKey;
 
@@ -53,16 +48,15 @@ public class UserController {
 
     // Add Users
     @PostMapping("/addUsers")
-    public ResponseEntity<HashMap<String,Object>> addUser(@RequestBody HashMap<String,String> req){
-
-        //Hashing function
+    public ResponseEntity<?> addUser(@RequestBody HashMap<String, String> req) {
+        // Hashing function
         PasswordHash psswrdHash = new PasswordHash();
-        //Response
-        HashMap<String,Object> resp = new HashMap<>();
+        // Response
+        HashMap<String, Object> resp = new HashMap<>();
         // Token Class
         TokenClass tokenClass = new TokenClass(secretKey);
 
-        if(userService.getUserFromUserName(req.get("UserName")).isPresent()){
+        if (userService.getUserFromUserName(req.get("UserName")).isPresent()) {
             resp.put("msg", "User Already Exists");
             return ResponseEntity.status(HttpStatus.CONFLICT).body(resp);
         }
@@ -72,86 +66,53 @@ public class UserController {
         newUser.setName(req.get("Name"));
         newUser.setUsername(req.get("UserName"));
         Patient_details newPatient = new Patient_details();
-        try{
+        try {
             newPatient = patientService.createPatient_details(newPatient);
             newUser.setPatient_details(newPatient);
             String psswrd = psswrdHash.getHash(req.get("PassWord"));
-            if(psswrd.equals("500")){
+            if (psswrd.equals("500")) {
                 resp.put("msg", "Internal Server Error");
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resp);
             }
             newUser.setPassword(psswrd);
 
-
             tokenClass.generateToken(newUser.getUID());
             newUser.setQrPath(CreateQr.generateAndSaveQRCode(psswrd, psswrd));
-        }catch(Exception err){
-            resp.put("msg", err);
+        } catch (Exception err) {
+            resp.put("msg", err.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resp);
         }
 
-        resp.put("msg", "User Not Created");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(resp);
+        resp.put("msg", "User Created Successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(resp);
     }
 
-    //Login
+    // Login
     @PostMapping("/userLogin")
-    public ResponseEntity<HashMap<String,Object>> userLogin(@RequestBody HashMap<String,String> req){
-        
-        //Hashing function
+    public ResponseEntity<HashMap<String, Object>> userLogin(@RequestBody HashMap<String, String> req) {
+        // Hashing function
         PasswordHash psswrdHash = new PasswordHash();
-        //response
-        HashMap<String,Object> resp = new HashMap<>();
+        // Response
+        HashMap<String, Object> resp = new HashMap<>();
 
         Optional<Users> user = userService.getUserFromUserName(req.get("UserName"));
-        if(!user.isPresent()){
+        if (!user.isPresent()) {
             resp.put("msg", "User Not Found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resp);
         }
 
-        if(user.get().getPassword().equals((psswrdHash.getHash(req.get("PassWord"))))){
-            resp.put("msg", "Login Sucessfull");
+        if (user.get().getPassword().equals((psswrdHash.getHash(req.get("PassWord"))))) {
+            resp.put("msg", "Login Successful");
             return ResponseEntity.status(HttpStatus.OK).body(resp);
         }
-        
+
         resp.put("msg", "Incorrect Password");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(resp);
     }
 
-    @GetMapping("/formFill")
-    @ResponseBody
-    public String getPatientDetails(@RequestParam(name = "token",required = false) String token, Model model){
-        if(token==null){
-            return "error";
-        }
-
-        TokenClass token1 = new TokenClass(secretKey);
-        if(token1.verifyToken(token)){
-            Optional<Patient_details> optionalPatient = patientService.getOnePatient( Integer.parseInt(token1.getPayload()));
-
-            if (!optionalPatient.isPresent()) {
-                return "error";
-            }
-    
-            Patient_details patient = optionalPatient.get();
-    
-            
-            model.addAttribute("firstname", patient.getFirst_name());
-            model.addAttribute("lastname", patient.getLast_name());
-            model.addAttribute("dateofbirth", patient.getDate_of_birth());
-            model.addAttribute("gender", patient.getGender());
-            model.addAttribute("medical_conditions", patient.getMedical_conditions());
-            model.addAttribute("medications", patient.getMedications());
-            model.addAttribute("allergies", patient.getAllergies());
-            model.addAttribute("last_appointment_date", patient.getLast_appointment_date());
-            model.addAttribute("phone_number", patient.getPhone_number());
-            model.addAttribute("email", patient.getEmail());
-            model.addAttribute("address", patient.getAddress());
-    
-            return "patientform";
-        }
-        return "error";
-        
+    @GetMapping("/getPatientDetails")
+    public String getPatientDetails(@RequestParam(name = "token", required = false) String token) {
+        return token;
     }
-
 }
+
