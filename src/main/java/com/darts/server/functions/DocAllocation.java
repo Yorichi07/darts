2b1678c072412252806c7f698ffd432b06a6c363
Lakshit <lakshitlucky11@gmail.com>
2024-04-15 23:34:11 +0530

    new

--- src/main/java/com/darts/server/functions/DocAllocation.java
+++ src/main/java/com/darts/server/functions/DocAllocation.java
@@ -1,53 +1,53 @@
-package com.darts.server.functions;
+// package com.darts.server.functions;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Random;
+// import java.util.stream.Collectors;
 
-import com.darts.server.model.Specialist;
+// import com.darts.server.model.Specialist;
 
-public class DocAllocation {
-    public Specialist allocateDoc(List<Specialist> specialists, int urgency, String searchTerm){
-        if (urgency == 1) {
-            return allocateEmergencyDoctor(specialists);
-        } else if (urgency == 0) {
-            return allocateNonEmergencyDoctor(specialists, searchTerm);
-        } else {
-            throw new IllegalArgumentException("Invalid urgency value. Must be 0 or 1.");
-        }
-    }
+// public class DocAllocation {
+//     public Specialist allocateDoc(List<Specialist> specialists, int urgency, String searchTerm){
+//         if (urgency == 1) {
+//             return allocateEmergencyDoctor(specialists);
+//         } else if (urgency == 0) {
+//             return allocateNonEmergencyDoctor(specialists, searchTerm);
+//         } else {
+//             throw new IllegalArgumentException("Invalid urgency value. Must be 0 or 1.");
+//         }
+//     }
     
 
-    public Specialist allocateEmergencyDoctor(List<Specialist> specialists){
-        List<Specialist> emerDoc=specialists.stream().filter(Specialist->"YES".equals(Specialist.getEmergency())).collect(Collectors.toList());
+//     public Specialist allocateEmergencyDoctor(List<Specialist> specialists){
+//         List<Specialist> emerDoc=specialists.stream().filter(Specialist->"YES".equals(Specialist.getEmergency())).collect(Collectors.toList());
 
-        if(emerDoc.isEmpty()){
-            return null;
-        }
+//         if(emerDoc.isEmpty()){
+//             return null;
+//         }
 
-        Random random = new Random();
-        int index=random.nextInt(emerDoc.size());
-        return emerDoc.get(index);
-    }
+//         Random random = new Random();
+//         int index=random.nextInt(emerDoc.size());
+//         return emerDoc.get(index);
+//     }
 
-    public Specialist allocateNonEmergencyDoctor(List<Specialist> specialists, String searchTerm) {
-        List<Specialist> filteredList = new ArrayList<>();
+//     public Specialist allocateNonEmergencyDoctor(List<Specialist> specialists, String searchTerm) {
+//         List<Specialist> filteredList = new ArrayList<>();
 
-        for (Specialist specialist : specialists) {
-            if ("NO".equalsIgnoreCase(specialist.getEmergency()) && 
-                specialist.getDocName().equalsIgnoreCase(searchTerm) ||
-                specialist.getSpeciality().equalsIgnoreCase(searchTerm)) {
-                filteredList.add(specialist);
-            }
-        }
+//         for (Specialist specialist : specialists) {
+//             if ("NO".equalsIgnoreCase(specialist.getEmergency()) && 
+//                 specialist.getDocName().equalsIgnoreCase(searchTerm) ||
+//                 specialist.getSpeciality().equalsIgnoreCase(searchTerm)) {
+//                 filteredList.add(specialist);
+//             }
+//         }
     
-        if (filteredList.isEmpty()) {
-            return null;
-        }
+//         if (filteredList.isEmpty()) {
+//             return null;
+//         }
 
-        Random random = new Random();
-        int index = random.nextInt(filteredList.size());
-        return filteredList.get(index);
-    }
-}
+//         Random random = new Random();
+//         int index = random.nextInt(filteredList.size());
+//         return filteredList.get(index);
+//     }
+// }
